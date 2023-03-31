b0089803c1ba0f267a12b302d7e2a3d6f54cc239
trungcx <trungcx143@gmail.com>
2023-03-31 15:41:37 +0700

    admin inventory add, delete ok

--- js/admin.js
+++ js/admin.js
@@ -14,8 +14,78 @@ function item() {
     this.type = 'item'
 }
 //Display item
-function displayItemInventory(){
+function displayItemInventory() {
+    let itemList = JSON.parse(localStorage.getItem('itemList'));
+    let inventory = document.getElementById('content-main');
+    let result = '';
+    if (itemList == null) {
+        inventory.innerHTML = 'No item.'
+        return false;
+    }
+    itemList.forEach((item, index) => {
+        result += `
+        <div class="item">
+                        <img class="item-image" src="${item.imgSource}" alt="">
+                        <div class="item-infor">
+                            <p class="infor-key">
+                                <strong><i>ID: </i></strong>
+                                <span class="infor-value">${item.itemId}</span>
+                            </p>
+                        </div>
+                        <div class="item-infor">
+                            <p class="infor-key">
+                                <strong><i>Name: </i></strong>
+                                <span class="infor-value">${item.itemName}</span>
+                            </p>
+                        </div>
+                        <div class="item-infor">
+                            <p class="infor-key">
+                                <strong><i>Category: </i></strong>
+                                <span class="infor-value">${item.category}</span>
+                            </p>
+                        </div>
+                        <div class="item-infor">
+                            <p class="infor-key">
+                                <strong><i>Price: </i></strong>
+                                <span class="infor-value">${item.price} $</span>
+                            </p>
+                        </div>
+                        <div class="item-infor item-infor-last">
+                            <p class="infor-key">
+                                <strong><i>Stock: </i></strong>
+                                <span class="infor-value">${item.stock}</span>
+                            </p>
+                        </div>
+                        <div class="button-wrapper">
+                            <div class="button delete-button" onclick="deleteItemFromItemList('${item.itemId}')">
+                                Delete
+                                <i class="fa-regular fa-trash-can"></i>
+                            </div>
+                            <div class="button edit-button">
+                                Edit
+                                <i class="fa-regular fa-pen-to-square"></i>
+                            </div>
+                        </div>
+                    </div>
+                `
+    })
+    inventory.innerHTML = result;
+}
+//Edit Item In Inventory
+function editItem(index){
     
+}
+//Delete Item In Inventory
+function deleteItemFromItemList(itemId){
+    console.log('delete here');
+    let itemList = JSON.parse(localStorage.getItem('itemList'));
+    let itemIdList = itemList.map((item,index)=>{
+        return item.itemId;
+    })
+    itemList.splice(itemIdList.indexOf(itemId),1);
+    localStorage.setItem('itemList',JSON.stringify(itemList));
+    displayItemInventory()
+    return true;
 }
 //Change image fnt
 function selectImage(image) {
@@ -28,7 +98,7 @@ function selectImage(image) {
     let imgSource = `../assets/img/${elementInput.name}`;
     localStorage.setItem('imgSourceOneTime', JSON.stringify(imgSource));
 }
-function addItemInforToItemList() { 
+function addItemInforToItemList() {
     //check blank
     let elementIdNameList = ['item-id', 'item-name', 'category', 'price', 'stock']; //we'll push imgSource after
     let itemInforArr = [];
@@ -61,6 +131,7 @@ function addItemInforToItemList() {
     itemList.push(itemNew);
     localStorage.setItem('itemList', JSON.stringify(itemList));
 
+    displayItemInventory();
     //Show message
     let message = "Add item successed"
     showSnackBar(message);
@@ -69,7 +140,7 @@ function addItemInforToItemList() {
     return itemList;
 }
 
-// Random itemID generator
+// Random item ID generator
 function randomItemIdGenerator() {
     let itemIdList = JSON.parse(localStorage.getItem('itemIdList'))
     let id;
